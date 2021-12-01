f764b1ae1bb4872de5a1cfcdaf31589acf334202
Lizeth <lztfuentespz@gmail.com>
2021-12-01 17:23:30 +0100

    add research interests

--- generate.py
+++ generate.py
@@ -485,7 +485,7 @@ class RenderContext(object):
                     self.SECTIONS_DIR, 'industry' + self._file_ending)
             elif section_tag in ['coursework', 'education', 'honors',
                                  'industry', 'research', 'skills', 'service',
-                                 'teaching', 'talks', 'advising']:
+                                 'teaching', 'talks', 'advising', 'interests']:
                 section_data['items'] = section_content
                 section_template_name = os.path.join(
                     self.SECTIONS_DIR, section_tag + self._file_ending)
